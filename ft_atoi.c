b15eae4cc4282f3f99c83850d717ed86a5742e09
Mateusz Zdrodowski <mzdrodow@student.42warsaw.pl>
2025-10-06 01:45:20 +0200

    atoiMOJE

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,40 +1,40 @@
 
-static int    ft_isspace(int c)
+static int	ft_isspace(int c)
 {
-    if (c == 9 || c == 10 || c == 11 || c == 12 || c == 13 || c == 32)
+	if (c == 9 || c == 10 || c == 11 || c == 12 || c == 13 || c == 32)
 
-        return (1);
-    return (0);
+		return (1);
+	return (0);
 }
 
-int    ft_atoi(const char *str)
+int	ft_atoi(const char *str)
 {
-    int    result;
-    int    sign;
-    int    i;
-    
-    result = 0;
-    sign = 1;
-    i = 0;
+	int result;
+	int sign;
+	int i;
 
-    while (ft_isspace(str[i]))
-        i++;
+	result = 0;
+	sign = 1;
+	i = 0;
 
-    if (str[i] == '+' && str[i + 1] != '-')
-        i++;
+	while (ft_isspace(str[i]))
+		i++;
 
-    if (str[i] == '-')
-    {
-        sign = -1;
-        i++;
-    }
-    while (str[i] && str[i] >= 48 && str[i] <= 57)
-    {
-        result *= 10;
-	result += str[i] - 48;
-	i++;
-    }
+	if (str[i] == '+' && str[i + 1] != '-')
+		i++;
 
-    result *= sign;
-    return (result);
+	if (str[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	while (str[i] && str[i] >= 48 && str[i] <= 57)
+	{
+		result *= 10;
+		result += str[i] - 48;
+		i++;
+	}
+
+	result *= sign;
+	return (result);
 }
