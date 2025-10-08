2076fd990a0b314672e82382e63424e23fc60f1d
Mateusz Zdrodowski <mzdrodow@student.42warsaw.pl>
2025-10-08 02:01:26 +0200

    prawiecalybonuszostallast

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mzdrodow <mzdrodow@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/06 02:05:09 by mzdrodow          #+#    #+#             */
-/*   Updated: 2025/10/08 00:46:55 by mzdrodow         ###   ########.fr       */
+/*   Updated: 2025/10/08 01:36:44 by mzdrodow         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -58,3 +58,6 @@ void				ft_lstadd_front(t_list **lst, t_list *new);
 int					ft_lstsize(t_list *lst);
 t_list				*ft_lstlast(t_list *lst);
 void				ft_lstadd_back(t_list **lst, t_list *new);
+void				ft_lstdelone(t_list *lst, void (*del)(void *));
+void				ft_lstclear(t_list **lst, void (*del)(void *));
+void				ft_lstiter(t_list *lst, void (*f)(void *));
